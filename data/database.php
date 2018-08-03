917eb4217e1e925a2b3e169a213ba9c1c6b067af
rock <383329495@qq.com>
2018-08-03 15:04:43 +0800

    V2.0

---
+++ data/database.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'database_type' => 'mysql',
+  'database_name' => 'calfbbs',
+  'server' => '127.0.0.1',
+  'username' => 'root',
+  'password' => '123456',
+  'charset' => 'utf8',
+  'port' => '3306',
+  'prefix' => 'calf_',
+);
